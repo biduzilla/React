9f93a6b1808cef09a0feed17cd385753b621f6d3
biduzilla <rlyowlly19@gmail.com>
2022-10-13 15:47:32 -0300

    banner

---
+++ react_alura/organo/src/components/banner/banner.js
@@ -0,0 +1,12 @@
+import './banner.css';
+
+function Banner() {
+    return (
+        <header className="banner">
+            <img src="/img/banner.png" alt="banner principal" />
+        </header>
+
+    )
+}
+
+export default Banner;
